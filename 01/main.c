585b6c0ba3f9014760fdce7609a2609dfbaa1484
axo4xo <axo@ax4.cz>
2026-01-25 16:30:35 +0100

    df

--- 01/main.c
+++ 01/main.c
@@ -1,12 +1,16 @@
 #include <stdint.h>
 #include <stdio.h>
 
+void print_hello() { printf("Hello, World!\n"); }
+
 int main(int argc, char *argv[]) {
-  printf("Hello, %s\n", argv[0]);
+  printf("Hello, %s\n", argv[1]);
 
   for (int i = 0; i < argc; i++) {
     printf("%d: %s\n", i, argv[i]);
   }
+
+  print_hello();
 
   return 0;
 }

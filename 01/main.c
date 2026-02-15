fd98ea045d783057f346c3270aa99bcf47289948
steveruu <axo@ax4.cz>
2026-02-15 17:33:21 +0100

    cs50

--- 01/main.c
+++ 01/main.c
@@ -1,16 +1,3 @@
-#include <stdint.h>
 #include <stdio.h>
 
-void print_hello() { printf("Hello, World!\n"); }
-
-int main(int argc, char *argv[]) {
-  printf("Hello, %s\n", argv[1]);
-
-  for (int i = 0; i < argc; i++) {
-    printf("%d: %s\n", i, argv[i]);
-  }
-
-  print_hello();
-
-  return 0;
-}
+int main(void) { printf("Hello, World!\n"); }

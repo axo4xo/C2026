3dc6910a393205d5333ac20817c1223eb6d07104
steveruu <axo@ax4.cz>
2026-02-15 21:26:03 +0100

    Update main.c

--- src/04-cat/main.c
+++ src/04-cat/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include "input_utils.h"
 
-void meow(void) {
-    printf("meow\n");
-}
+void meow(size_t n);
 
 int main(void) {
-    // size_t je tady unsigned ! lepsi
-    for (size_t i = 0; i < 3; i++)
-    {
-       meow();
-    }
+    int n = get_int("Kolikrat mam zamnoukat? ");
+    meow(n);
 }
 
+void meow(size_t n) {
+    // size_t je tady unsigned ! lepsi
+    for (size_t i = 0; i < n; i++) {
+        printf("meow\n");
+    }
+}

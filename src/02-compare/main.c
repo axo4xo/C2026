cbdbb5ab6d6474ae6982efdebafd3d6b8623a3e5
steveruu <axo@ax4.cz>
2026-02-15 20:14:38 +0100

    Update main.c

--- src/02-compare/main.c
+++ src/02-compare/main.c
@@ -4,4 +4,12 @@
 int main(void) {
     int x = get_int("Zadej x: ");
     int y = get_int("Zadej y: ");
+
+    if (x < y) {
+        printf("%i je menší než %i\n", x, y);
+    } else if (x > y) {
+        printf("%i je větší než %i\n", x, y);
+    } else {
+        printf("%i a %i jsou stejné\n", x, y);
+    }
 }

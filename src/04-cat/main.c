90497c2f55bf4340e4157e82daf6b2e8db14a083
steveruu <axo@ax4.cz>
2026-02-15 22:51:21 +0100

    Update main.c

--- src/04-cat/main.c
+++ src/04-cat/main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include "input_utils.h"
 
-
 void meow(size_t n);
+int get_positive_int(void);
 
 int main(void) {
-    int n = 1;
+    int32_t n = get_positive_int();
+
+    meow(n);
+}
+
+int get_positive_int(void) {
+    int32_t n;
     do {
         n = get_int("Kolikrat mam zamnoukat? ");
     } while (n <= 0);
-
-    meow(n);
+    return n;
 }
 
 void meow(size_t n) {

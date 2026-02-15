b6acd929315d48948ed6103c0889d769a179d9a4
steveruu <axo@ax4.cz>
2026-02-15 23:21:19 +0100

    feat: print horizontally

--- src/05-mario/main.c
+++ src/05-mario/main.c
@@ -3,11 +3,14 @@
 // size_t - signed, dává větší smysl
 void print_horizontally(size_t, char);
 void print_vertically(size_t, char);
+void print_block(size_t x, size_t y, char);
 
 int main(void) {
     print_horizontally(7, '?');
-    printf("---\n");
+    printf("----------------\n");
     print_vertically(5, '#');
+    printf("----------------\n");
+    print_block(6, 3, '*');
 }
 
 void print_horizontally(size_t n, char symbol) {
@@ -21,5 +24,11 @@ void print_horizontally(size_t n, char symbol) {
 void print_vertically(size_t n, char symbol) {
     for (size_t i = 0; i < n; i++) {
         printf("%c\n", symbol);
+    }
+}
+
+void print_block(size_t x, size_t y, char symbol) {
+    for (size_t i = 0; i < y; i++) {
+        print_horizontally(x, symbol);
     }
 }

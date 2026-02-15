acebfeff26e067698e7424c7bbda9dd9f9dc199e
steveruu <axo@ax4.cz>
2026-02-15 23:31:53 +0100

    Update main.c

--- src/05-mario/main.c
+++ src/05-mario/main.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 
 // size_t - unsigned, dává větší smysl
-void print_horizontally(size_t, char);
-void print_vertically(size_t, char);
+typedef enum {
+    HORIZONTALLY,
+    VERTICALLY,
+} Orientation;
+
 void print_block(size_t x, size_t y, char);
+void print_symbols(size_t x, char, Orientation);
 
 int main(void) {
-    print_horizontally(7, '?');
-    printf("----------------\n");
-    print_vertically(5, '#');
-    printf("----------------\n");
-    print_block(6, 3, '*');
+    print_symbols(7, '?', HORIZONTALLY);
+    printf("\n\n");
+    print_symbols(5, '#', VERTICALLY);
+    printf("\n\n");
+    print_block(6, 3, 'H');
 }
 
-void print_horizontally(size_t n, char symbol) {
+void print_symbols(size_t n, char symbol, Orientation dir) {
     for (size_t i = 0; i < n; i++)
     {
-        printf("%c", symbol);
+        dir == HORIZONTALLY ? printf("%c", symbol) : printf("%c\n", symbol);
     }
-    printf("\n");
-}
-
-void print_vertically(size_t n, char symbol) {
-    for (size_t i = 0; i < n; i++) {
-        printf("%c\n", symbol);
-    }
+    dir == HORIZONTALLY && printf("\n");
 }
 
 void print_block(size_t x, size_t y, char symbol) {
     for (size_t i = 0; i < y; i++) {
-        print_horizontally(x, symbol);
+        print_symbols(x, symbol, HORIZONTALLY);
     }
 }

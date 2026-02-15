263de335a3119d7f6658fd66868f83118b8eb7f2
steveruu <axo@ax4.cz>
2026-02-15 20:42:44 +0100

    Update main.c

--- src/03-agree/main.c
+++ src/03-agree/main.c
@@ -3,5 +3,5 @@
 
 int main(void) {
     char agreement = get_char("Do you agree? ");
-    agreement == 'Y' ? printf(":)\n") : printf(":(\n");
+    agreement == 'y' ? printf(":)\n") : printf(":(\n");
 }
